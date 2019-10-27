b079e2b30901e496cc3c98e972d68dfed0ae5922
Steph Ridnell <steph.ridnell@gmail.com>
2019-10-27 13:54:06 +1100

    Fixed indenting

--- app/kudo/service.py
+++ app/kudo/service.py
@@ -10,41 +10,41 @@ class Service(object):
     if not user_id:
       raise Exception("User id not provided")
 
-    def find_all_kudos(self):
-      kudos  = self.repo_client.find_all({
-        'user_id': self.user_id
+  def find_all_kudos(self):
+    kudos  = self.repo_client.find_all({
+      'user_id': self.user_id
+    })
+    return [self.dump(kudo) for kudo in kudos]
+
+  def find_kudo(self, repo_id):
+    kudo = self.repo_client.find({
+      'user_id': self.user_id,
+      'repo_id': repo_id
       })
-      return [self.dump(kudo) for kudo in kudos]
+    return self.dump(kudo)
 
-    def find_kudo(self, repo_id):
-      kudo = self.repo_client.find({
-        'user_id': self.user_id,
-        'repo_id': repo_id
-        })
-      return self.dump(kudo)
+  def create_kudo_for(self, githubRepo):
+    self.repo_client.create(self.prepare_kudo(githubRepo))
+    return self.dump(githubRepo.data)
 
-    def create_kudo_for(self, githubRepo):
-      self.repo_client.create(self.prepare_kudo(githubRepo))
-      return self.dump(githubRepo.data)
+  def update_kudo_with(self, repo_id, githubRepo):
+    records_affected = self.repo_client.update({
+      'user_id': self.user_id,
+      'repo_id': repo_id
+      }, self.prepare_kudo(githubRepo))
+    return records_affected > 0
 
-    def update_kudo_with(self, repo_id, githubRepo):
-      records_affected = self.repo_client.update({
-        'user_id': self.user_id,
-        'repo_id': repo_id
-        }, self.prepare_kudo(githubRepo))
-      return records_affected > 0
+  def delete_kudo_for(self, repo_id):
+    records_affected = self.repo_client.delete({
+      'user_id': self.user_id,
+      'repo_id': repo_id
+    })
+    return records_affected > 0
 
-    def delete_kudo_for(self, repo_id):
-      records_affected = self.repo_client.delete({
-        'user_id': self.user_id,
-        'repo_id': repo_id
-      })
-      return records_affected > 0
+  def dump(self, data):
+    return KudoSchema(exclude=['_id']).dump(data).data
 
-    def dump(self, data):
-      return KudoSchema(exclude=['_id']).dump(data).data
-
-    def prepare_kudo(self, githubRepo):
-      data = githubRepo.data
-      data['user_id'] = self.user_id
-      return data
+  def prepare_kudo(self, githubRepo):
+    data = githubRepo.data
+    data['user_id'] = self.user_id
+    return data
